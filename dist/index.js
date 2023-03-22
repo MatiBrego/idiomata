14198bc4fbef30a4286139d640735f0035ea6c33
MatiBrego <matias.bregoli@ing.austral.edu.ar>
2023-03-22 09:47:25 -0300

    Prueba axios

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
 const router_1 = require("./router/router");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use((0, cors_1.default)());
 app.use('/api', router_1.router);
-app.listen(8000, () => {
-    console.log('[server]: Server is running at http://localhost:8000');
+app.get('/', (req, res) => res.send('Hi mom'));
+app.listen(3001, () => {
+    console.log('[server]: Server is running at http://localhost:3001');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { router } from "./router/router";
+import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
 
 app.use('/api', router)
+app.get('/', (req, res)=>res.send('Hi mom'))
 
-app.listen(8000, ()=>{
-    console.log('[server]: Server is running at http://localhost:8000')
+app.listen(3001, ()=>{
+    console.log('[server]: Server is running at http://localhost:3001')
 })

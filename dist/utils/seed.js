afea9d70ac60eb8ac60e002c61d2efb13db4e492
Matias Bregoli <matiasbregoli@sirius.com.ar>
2023-07-16 18:29:30 -0300

    Changed seed

--- dist/utils/seed.js
+++ dist/utils/seed.js
@@ -19,16 +19,15 @@ function resetDb() {
         yield db_1.db.user.deleteMany();
         yield db_1.db.word.deleteMany();
         yield db_1.db.wordAttempt.deleteMany();
+        yield db_1.db.sentence.deleteMany();
         const languagesId = yield createLanguages();
         createUsers();
-        const categoryIds = yield createCategories();
-        const wordsId = yield createWords(categoryIds);
-        createTranslations(wordsId, languagesId);
+        yield createCategories();
     });
 }
 exports.resetDb = resetDb;
 //Creates 5 users who have:
-// name: useri  password: 1  email: useri@gmail.com  lannguage: spanish
+// name: useri  password: i  email: useri@gmail.com  lannguage: spanish
 function createUsers() {
     return __awaiter(this, void 0, void 0, function* () {
         for (let i = 1; i < 6; i++) {
@@ -54,57 +53,65 @@ function createLanguages() {
         const languagesId = [];
         languagesId.push((yield db_1.db.language.create({ data: { name: 'spanish' } })).id);
         languagesId.push((yield db_1.db.language.create({ data: { name: 'italian' } })).id);
+        languagesId.push((yield db_1.db.language.create({ data: { name: 'french' } })).id);
         return languagesId;
     });
 }
 function createCategories() {
     return __awaiter(this, void 0, void 0, function* () {
-        const home = yield db_1.db.category.create({
+        yield db_1.db.category.create({
             data: { name: "home" }
         });
-        const school = yield db_1.db.category.create({
+        yield db_1.db.category.create({
             data: { name: "school" }
         });
-        return [home.id, school.id];
-    });
-}
-function createWords(categoryIds) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const wordsId = [];
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "chair", categoryId: categoryIds[0] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "bathroom", categoryId: categoryIds[0] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "desk", categoryId: categoryIds[0] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "window", categoryId: categoryIds[0] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "door", categoryId: categoryIds[0] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "pencil", categoryId: categoryIds[1] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "blackboard", categoryId: categoryIds[1] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "teacher", categoryId: categoryIds[1] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "student", categoryId: categoryIds[1] } }));
-        wordsId.push(yield db_1.db.word.create({ data: { inEnglish: "schoolbag", categoryId: categoryIds[1] } }));
-        return wordsId.map((word) => word.id);
-    });
-}
-function createTranslations(wordsId, languagesId) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield db_1.db.translation.create({ data: { wordId: wordsId[0], translated: "silla", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[0], translated: "sedia", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[1], translated: "baño", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[1], translated: "bagno", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[2], translated: "escritorio", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[2], translated: "scrivania", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[3], translated: "ventana", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[3], translated: "finestra", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[4], translated: "puerta", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[4], translated: "porta", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[5], translated: "lápiz", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[5], translated: "matita", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[6], translated: "pizarra", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[6], translated: "lavagna", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[7], translated: "profesor", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[7], translated: "insegnante", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[8], translated: "estudiante", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[8], translated: "studente", languageId: languagesId[1], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[9], translated: "mochila", languageId: languagesId[0], difficulty: "EASY" } });
-        yield db_1.db.translation.create({ data: { wordId: wordsId[9], translated: "zaino", languageId: languagesId[1], difficulty: "EASY" } });
+        yield db_1.db.category.create({
+            data: { name: "food" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "vehicle" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "technology" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "music" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "kitchen" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "clothing" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "nature" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "celestial" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "communication" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "sports" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "flower" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "furniture" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "geography" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "animal" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "movie" }
+        });
+        yield db_1.db.category.create({
+            data: { name: "beverage" }
+        });
     });
 }
--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -7,16 +7,15 @@ export async function resetDb(){
     await db.user.deleteMany()
     await db.word.deleteMany()
     await db.wordAttempt.deleteMany()
+    await db.sentence.deleteMany()
 
     const languagesId = await createLanguages()
     createUsers()
-    const categoryIds = await createCategories()
-    const wordsId = await createWords(categoryIds)
-    createTranslations(wordsId, languagesId)  
+    await createCategories()
 }
 
 //Creates 5 users who have:
-// name: useri  password: 1  email: useri@gmail.com  lannguage: spanish
+// name: useri  password: i  email: useri@gmail.com  lannguage: spanish
 async function createUsers(){
     for (let i = 1; i < 6; i++) {
         await db.user.create({
@@ -42,60 +41,66 @@ async function createLanguages(){
 
     languagesId.push((await db.language.create({data: {name: 'spanish'}})).id)
     languagesId.push((await db.language.create({data: {name: 'italian'}})).id)
+    languagesId.push((await db.language.create({data: {name: 'french'}})).id)
 
     return languagesId
 }
 
 async function createCategories() {
 
-    const home = await db.category.create({
+    await db.category.create({
         data: {name: "home"}
     })
-    const school = await db.category.create({
+    await db.category.create({
         data: {name: "school"}
     })
-
-    return [home.id, school.id]
-}
-
-
-async function createWords(categoryIds: number[]){
-    const wordsId = []
-
-    wordsId.push(await db.word.create({data: {inEnglish: "chair", categoryId: categoryIds[0]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "bathroom", categoryId: categoryIds[0]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "desk", categoryId: categoryIds[0]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "window", categoryId: categoryIds[0]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "door", categoryId: categoryIds[0]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "pencil", categoryId: categoryIds[1]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "blackboard", categoryId: categoryIds[1]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "teacher", categoryId: categoryIds[1]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "student", categoryId: categoryIds[1]}}))
-    wordsId.push(await db.word.create({data: {inEnglish: "schoolbag", categoryId: categoryIds[1]}}))
-
-    return wordsId.map((word) => word.id)
-}
-
-async function createTranslations(wordsId: number[], languagesId: number[]) {
-    await db.translation.create({data: {wordId: wordsId[0], translated: "silla", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[0], translated: "sedia", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[1], translated: "baño", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[1], translated: "bagno", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[2], translated: "escritorio", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[2], translated: "scrivania", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[3], translated: "ventana", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[3], translated: "finestra", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[4], translated: "puerta", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[4], translated: "porta", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[5], translated: "lápiz", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[5], translated: "matita", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[6], translated: "pizarra", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[6], translated: "lavagna", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[7], translated: "profesor", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[7], translated: "insegnante", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[8], translated: "estudiante", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[8], translated: "studente", languageId: languagesId[1], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[9], translated: "mochila", languageId: languagesId[0], difficulty: "EASY"}})
-    await db.translation.create({data: {wordId: wordsId[9], translated: "zaino", languageId: languagesId[1], difficulty: "EASY"}})
+    await db.category.create({
+        data: {name: "food"}
+    })
+    await db.category.create({
+        data: {name: "vehicle"}
+    })
+    await db.category.create({
+        data: {name: "technology"}
+    })
+    await db.category.create({
+        data: {name: "music"}
+    })
+    await db.category.create({
+        data: {name: "kitchen"}
+    })
+    await db.category.create({
+        data: {name: "clothing"}
+    })
+    await db.category.create({
+        data: {name: "nature"}
+    })
+    await db.category.create({
+        data: {name: "celestial"}
+    })
+    await db.category.create({
+        data: {name: "communication"}
+    })
+    await db.category.create({
+        data: {name: "sports"}
+    })
+    await db.category.create({
+        data: {name: "flower"}
+    })
+    await db.category.create({
+        data: {name: "furniture"}
+    })
+    await db.category.create({
+        data: {name: "geography"}
+    })
+    await db.category.create({
+        data: {name: "animal"}
+    })
+    await db.category.create({
+        data: {name: "movie"}
+    })
+    await db.category.create({
+        data: {name: "beverage"}
+    })
 }
 
